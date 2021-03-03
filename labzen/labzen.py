17cfdee7b0f6e1ce44875205dc681fc6b80664e8
Rafael Pilliard-Hellwig <rafael.taph@gmail.com>
2021-03-03 13:56:52 -0800

    Add initial work on count_points()

--- labzen/labzen.py
+++ labzen/labzen.py
@@ -1,4 +1,7 @@
+import os
+from pathlib import Path
 import pandas as pd
+import numpy as np
 
 
 def parse_lab(file_name: str) -> list:
@@ -19,6 +22,8 @@ def parse_lab(file_name: str) -> list:
 
 
 def count_points(file_name: str = None) -> pd.DataFrame:
+
+def count_points(file_name: str = None, margins: bool = True) -> pd.DataFrame:
     """Tally Available Points in Lab
 
     Args:
@@ -34,11 +39,37 @@ def count_points(file_name: str = None) -> pd.DataFrame:
     Example:
         count_points()
     """
-    return None
+    # Calculate a summary table of points per section
+    res = parse_lab(file_name)
+    df = pd.DataFrame({"block": np.arange(1, len(res) + 1), "txt": res})
+    df["txt"] = df["txt"].str.replace("<br>", "\n")
+    df["txt"] = df["txt"].str.split("\n")
+    df = df.explode("txt")
+    df["txt"] = df["txt"].replace(["", "<hr>"], np.nan)
+    df = df.dropna()
+    df["txt"] = df["txt"].str.strip()
+    df["header"] = df["txt"].shift(1)
+    df["rubric"] = df["txt"].str.contains(r"^rubric\=\{")
+    df["below_header"] = df["header"].str.contains(r"^[#]{1,6}\s")
+    df["optional"] = df["header"].str.contains("optional|bonus", case=False)
+    df = df.dropna().query("rubric")
+    df["points"] = df["txt"].str.findall(r"(\d+)")
+    df2 = df.explode("points")
+    df2["points"] = df2["points"].astype(int)
+    df["points"] = df2["points"].groupby(df2.index).apply(list)
+    df["total"] = df["points"].apply(sum)
+    booldict = {True: "Optional", False: "Non-Optional"}
+    df["type"] = df["optional"].replace(booldict)
+    df.drop(columns=["rubric", "below_header", "optional"])
+    df = df.reset_index(drop=True)
+
+    # Generate crosstab
+    tab = df.pivot_table("total", "type", aggfunc=sum, margins=margins).reset_index()
+    return tab
 
 
 def check_repo_link(file_name: str) -> bool:
-    """ Check whether the user has included the github repo link in his/her 
+    """Check whether the user has included the github repo link in his/her
         repository
 
     Args:
@@ -48,16 +79,16 @@ def check_repo_link(file_name: str) -> bool:
             extension.
 
     Returns:
-        bool: a boolean output 
+        bool: a boolean output
 
     Example:
-        check_repo_link()    
+        check_repo_link()
     """
     return None
 
 
 def check_lat_version(file_name: str) -> bool:
-    """ Check whether the user has pushed the latest version in his/her 
+    """Check whether the user has pushed the latest version in his/her
         repository
 
     Args:
@@ -70,13 +101,13 @@ def check_lat_version(file_name: str) -> bool:
         bool: a boolean output
 
     Example:
-        check_lat_version() 
+        check_lat_version()
     """
     return None
 
 
 def check_commits(file_name: str) -> bool:
-    """ Check whether the user has at least three commits
+    """Check whether the user has at least three commits
 
     Args:
         file_name (str): A path or list of paths to MDS lab files (either
@@ -88,15 +119,15 @@ def check_commits(file_name: str) -> bool:
         bool: a boolean output
 
     Example:
-        check_commits()      
+        check_commits()
     """
     return None
 
 
 def check_mechanics(file_name: str) -> NoneType:
     """Performs Mechanics Checks on a MDS Lab
-       This function check that you have a Github repo link, that you have 
-       pushed your latest commit, and that you have at least three commit 
+       This function check that you have a Github repo link, that you have
+       pushed your latest commit, and that you have at least three commit
        messages authored by you in your history.
 
     Args:
@@ -109,8 +140,8 @@ def check_mechanics(file_name: str) -> NoneType:
         NoneType: The function prints the results of the mechanics checks to screen
 
     Example:
-        check_mechanics()      
-    
+        check_mechanics()
+
     """
     return None
 
