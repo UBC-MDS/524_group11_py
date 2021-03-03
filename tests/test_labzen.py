0e1324560bf6a8cdde0ec1e2c7ecd7a1f80a9c23
Rafael Pilliard-Hellwig <rafael.taph@gmail.com>
2021-03-03 13:47:56 -0800

    Add initial unit tests for count_points()

--- tests/test_labzen.py
+++ tests/test_labzen.py
@@ -1,5 +1,37 @@
 from labzen import __version__
-from labzen import labzen
+from labzen import labzen as lz
+from pathlib import Path
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_count_points_type():
+    # Test that the return type is a dataframe
+    labfile = Path("data-raw/dummylab.ipynb")
+    tab = lz.count_points(labfile, margins=False)
+    assert type(tab) == "pandas.core.frame.DataFrame"
+
+
+def test_ipynb_points_2_rows():
+    labfile = Path("data-raw/dummylab.ipynb")
+    tab = lz.count_points(labfile, margins=False)
+    assert tab.shape == (2, 2)
+
+
+def test_ipynb_points_2_rows():
+    labfile = Path("data-raw/dummylab.ipynb")
+    tab = lz.count_points(labfile, margins=True)
+    assert tab.shape == (3, 2)
+
+
+def test_rmd_points_2_rows():
+    labfile = Path("data-raw/dummylab.rmd")
+    tab = lz.count_points(labfile, margins=False)
+    assert tab.shape == (2, 2)
+
+
+def test_rmd_points_2_rows():
+    labfile = Path("data-raw/dummylab.rmd")
+    tab = lz.count_points(labfile, margins=True)
+    assert tab.shape == (3, 2)
