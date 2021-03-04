d9995e61d0250e4a2998606935dae0e1f6aaa60d
Rafael Pilliard-Hellwig <rafael.taph@gmail.com>
2021-03-03 22:22:48 -0800

    Comment count_points() and return tuple

--- labzen/labzen.py
+++ labzen/labzen.py
@@ -21,9 +21,7 @@ def parse_lab(file_name: str) -> list:
     return None
 
 
-def count_points(file_name: str = None) -> pd.DataFrame:
-
-def count_points(file_name: str = None, margins: bool = True) -> pd.DataFrame:
+def count_points(file_name: str = None, margins: bool = True):
     """Tally Available Points in Lab
 
     Args:
@@ -31,41 +29,59 @@ def count_points(file_name: str = None, margins: bool = True) -> pd.DataFrame:
             .ipynb or .Rmd). If left blank, the function will recursively
             search for all labs in the working directory based on the file
             extension.
+        margins (bool): A boolean indicating whether to add a row for the
+            total number of points (optional + required). Defaults to True.
 
     Returns:
-        pd.DataFrame: A cross table summarising the number of optional,
-            required, and total points per lab.
+        (pandas.core.frame.DataFrame, pandas.core.frame.DataFrame):
+            A tuple of DataFrames. The first is a section-by-section overview
+            of points available. The second is a cross table summarising the
+            number of optional, required, and total points per lab.
 
     Example:
-        count_points()
+        # Navigate to an MDS lab directory and run:
+        df, tab = count_points()
+        print(df)
+        print(tab)
     """
-    # Calculate a summary table of points per section
+    # Parse a lab file into its markdown blocks
     res = parse_lab(file_name)
     df = pd.DataFrame({"block": np.arange(1, len(res) + 1), "txt": res})
+
+    # Tidy breaks, new lines, extra spaces, and make each line a row
     df["txt"] = df["txt"].str.replace("<br>", "\n")
     df["txt"] = df["txt"].str.split("\n")
     df = df.explode("txt")
     df["txt"] = df["txt"].replace(["", "<hr>"], np.nan)
     df = df.dropna()
     df["txt"] = df["txt"].str.strip()
+
+    # Add variable transformations
     df["header"] = df["txt"].shift(1)
     df["rubric"] = df["txt"].str.contains(r"^rubric\=\{")
     df["below_header"] = df["header"].str.contains(r"^[#]{1,6}\s")
     df["optional"] = df["header"].str.contains("optional|bonus", case=False)
+
+    # Subset to lines containing rubrics only
     df = df.dropna().query("rubric")
+
+    # Extract and sum points
     df["points"] = df["txt"].str.findall(r"(\d+)")
     df2 = df.explode("points")
     df2["points"] = df2["points"].astype(int)
     df["points"] = df2["points"].groupby(df2.index).apply(list)
     df["total"] = df["points"].apply(sum)
+
+    # Tidy and make the result more human-readable
     booldict = {True: "Optional", False: "Non-Optional"}
     df["type"] = df["optional"].replace(booldict)
-    df.drop(columns=["rubric", "below_header", "optional"])
+    df = df.drop(columns=["rubric", "below_header", "optional"])
     df = df.reset_index(drop=True)
 
     # Generate crosstab
-    tab = df.pivot_table("total", "type", aggfunc=sum, margins=margins).reset_index()
-    return tab
+    tab = df.pivot_table("total", "type", aggfunc=sum, margins=margins)
+    tab = tab.reset_index()
+    return df, tab
 
 
 def check_repo_link(file_name: str) -> bool:
